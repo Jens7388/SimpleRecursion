208af711dad539476c2595799f7bc6afd9293cba
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2021-01-18 13:13:27 +0100

    Added Faculty

---
+++ Faculty/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Faculty
+{
+    class Program
+    {
+        static void Main()
+        {
+            for(int n = 1; n < 15; n++)
+            {
+                Console.WriteLine(Faculty(n));
+            }
+            Console.ReadLine();
+        }
+
+        private static int Faculty(int n)
+        {
+            if(n == 1)
+            {
+                return n;
+            }
+            else
+            {
+                return n * Faculty(n-1);
+            }
+        }
+    }
+}
