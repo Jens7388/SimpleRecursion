aac581b27bcda22ee40f29242c826c83b531569e
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2021-01-18 09:35:51 +0100

    Add project files.

---
+++ SimpleRecursion/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SimpleRecursion
+{
+    class Program
+    {
+        static void Main()
+        {
+            int SimpleRecursion(int n)
+            {
+                if(n == 0)  // Base case
+                    return -1;
+                else        // Recursive case
+                    return SimpleRecursion(n - 1);
+            }
+            Console.WriteLine(SimpleRecursion(2));
+            Console.ReadLine();
+        }
+    }
+}
