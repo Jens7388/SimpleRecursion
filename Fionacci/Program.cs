1cfbf6108a48ebacfaa91286d1c1a7b531d0c527
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2021-01-18 11:39:34 +0100

    added Fibonacci

---
+++ Fionacci/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Fionacci
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            FibonacciSequence();
+        }
+
+        private static void FibonacciSequence()
+        {
+            for(int n = 2; n < 45; n++)
+            {
+                Stopwatch timer = new Stopwatch();
+                timer.Start();
+                int result = Fibonacci(n);
+                timer.Stop();
+                Console.WriteLine($"{n}, {result} Elapsed time: {timer.Elapsed}");
+                timer.Reset();            
+            }
+            Console.ReadLine();
+        }
+
+        private static int Fibonacci(int n)
+        {
+            if(n < 2)
+            {
+                return n;
+            }
+            else
+            {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+        }
+    }
+}
--- SimpleRecursion/Program.cs
+++ SimpleRecursion/Program.cs
@@ -5,16 +5,21 @@ namespace SimpleRecursion
     class Program
     {
         static void Main()
-        {
-            int SimpleRecursion(int n)
-            {
-                if(n == 0)  // Base case
-                    return -1;
-                else        // Recursive case
-                    return SimpleRecursion(n - 1);
-            }
-            Console.WriteLine(SimpleRecursion(2));
+        {   
+            Console.WriteLine(SimpleRecursion(3)); 
             Console.ReadLine();
+        }
+
+        private static int SimpleRecursion(int n)
+        {
+            if(n == 0)  // Base case             
+            {
+                return -1;
+            }
+            else        // Recursive case                 
+            {
+                return SimpleRecursion(n - 1);
+            }
         }
     }
 }
